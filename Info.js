b5878f2b4934e1357d1e467e61df3ddc928c7c71
JurrijnP <slubadog@gmail.com>
2017-09-23 14:04:43 +0200

    Update Info.js

--- Info.js
+++ Info.js
@@ -5,13 +5,9 @@ function Info() {
     Embed = "{embed:\
       {type:rich}\
       {color:#bb29f9}\
-      {title:	𝖄𝖔𝖚 𝖍𝖆𝖛𝖊 𝖗𝖊𝖖𝖚𝖊𝖘𝖙𝖊𝖉 𝖞𝖔𝖚𝖗 𝖈𝖎𝖙𝖞 𝖉𝖆𝖙𝖆:}\
+      {title:𝖄𝖔𝖚 𝖍𝖆𝖛𝖊 𝖗𝖊𝖖𝖚𝖊𝖘𝖙𝖊𝖉 𝖞𝖔𝖚𝖗 𝖈𝖎𝖙𝖞 𝖉𝖆𝖙𝖆:}\
       {desc:\
-𝕮𝖆𝖘𝖍\: *" + prs(echonia[UserID])["Cash"] + "*\
-𝕭𝖆𝖓𝖐\: *" + prs(echonia[UserID])["Bank"] + "*\
-𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊\: *" + prs(echonia[UserID])["Experience"] + "*\
-𝕷𝖊𝖛𝖊𝖑\: *" + prs(echonia[UserID])["Level"] + "*\
-𝕮𝖎𝖙𝖞\: *" + prs(echonia[UserID])["City"] + "*\
+𝕮𝖆𝖘𝖍: *" + prs(echonia[UserID])["Cash"] + "*\n𝕭𝖆𝖓𝖐: *" + prs(echonia[UserID])["Bank"] + "*\n𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊: *" + prs(echonia[UserID])["Experience"] + "*\n𝕷𝖊𝖛𝖊𝖑: *" + prs(echonia[UserID])["Level"] + "*\n𝕮𝖎𝖙𝖞: *" + prs(echonia[UserID])["City"] + "*\
       }\
     }";
   return Embed;
