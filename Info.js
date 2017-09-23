e1e3e625b5e8a44ee88561805863008d03579a5a
JurrijnP <slubadog@gmail.com>
2017-09-23 13:59:46 +0200

    Create Info.js

---
+++ Info.js
@@ -0,0 +1,17 @@
+function Info() {
+  use echonia;
+  if (echonia[UserID]) {
+    resp = "{embed:\
+      {type:rich}\
+      {color:#bb29f9}\
+      {title:	𝖄𝖔𝖚 𝖍𝖆𝖛𝖊 𝖗𝖊𝖖𝖚𝖊𝖘𝖙𝖊𝖉 𝖞𝖔𝖚𝖗 𝖈𝖎𝖙𝖞 𝖉𝖆𝖙𝖆:}\
+      {desc:\
+𝕮𝖆𝖘𝖍: *" + prs(echonia[UserID])["Cash"] + "*\
+𝕭𝖆𝖓𝖐: *" + prs(echonia[UserID])["Bank"] + "*\
+𝕰𝖝𝖕𝖊𝖗𝖎𝖊𝖓𝖈𝖊: *" + prs(echonia[UserID])["Experience"] + "*\
+𝕷𝖊𝖛𝖊𝖑: *" + prs(echonia[UserID])["Level"] + "*\
+𝕮𝖎𝖙𝖞: *" + prs(echonia[UserID])["City"] + "*\
+      }\
+    }";
+  };
+};
